8d17e7594760b9a7bf7669c2fe2cbbb9142d6938
Felipe Alves <usermestre@hotmail.com>
2021-02-24 14:02:35 -0300

    Class 3 46min

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,39 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../context/ChallengeContext';
+import styles from '../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+  const ContextData = useContext(ChallengesContext);
+  console.log(ContextData)
+  
+  const hasActiveChallenge = true;
+
+  return(
+    <div className={styles.ChallengeBoxContainer}>
+
+      { hasActiveChallenge ? (
+        <div className={styles.ChallengeActive}>
+          <header>Ganhe 400 xp</header>
+          <main>
+              <img src="icons/body.svg"/>
+              <strong>Novo Desafio</strong>
+              <p>Levante e faça uma caminhada de 3 minutos.</p>
+          </main>
+
+          <footer>
+              <button type="button" className={styles.challengeFailedButton}>Falhei</button>
+              <button type="button" className={styles.challengeSucessButton}>Completei</button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.ChallengeNotActive}>
+          <strong>Finalize um ciclo para receber um desafio.</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up"/>
+            Avance de level completando todos os desafios disponíveis.
+          </p>
+        </div>
+      ) }
+    </div>
+  )
+}
